4dc5f493ff288de620dd10b4b9328bcc38a3dd1c
NathanWalshNCI <x22154086@student.ncirl.ie>
2023-04-04 12:39:41 +0100

    added continuous delivery

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+sudo apt update && sudo apt install nodejs npm
+#Install PM2 which is a production process manager for Node.js with a built-in load balancer.
+sudo npm install -g pm2
+#Stop any instance of our application running currently
+pm2 stop example_app
+pm2 delete example_app
+#CD into folder where application is downloaded
+cd ProjectDevOpsTest/
+#Install application dependencies
+npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
+#Start the application with the process name example_app using PM2
+pm2 start node_modules/react-scripts/scripts/start.js --name example_app
